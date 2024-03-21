54ed293f1ea824203f04c99e489280107677a8f0
Kh.Eliash <eliashebrahim@gmail.com>
2024-03-21 20:04:25 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+import Home from "./Pages/Home";
+
+function App() {
+  return (
+    <>
+      <Home></Home>
+
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import Nav from "../Components/Nav";
+
+const Home = () => {
+  return (
+    <div className="">
+      <div className="h-16">
+        <Nav></Nav>
+      </div>
+      <h2> Home</h2>
+    </div>
+  );
+};
+
+export default Home;
